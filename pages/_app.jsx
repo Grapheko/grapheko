cdcef9a510644f565fbab92a8f712fe55aa95ebe
Grapheko <admin@grapheko.fr>
2026-05-11 17:27:35 +0200

    Update _app.jsx
    update app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import Cursor from '../components/ui/Cursor'
+import Nav from '../components/layout/Nav'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Cursor />
+      <Nav />
       <Component {...pageProps} />
     </>
   )
